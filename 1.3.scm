e9ab3cd249413d2d88c38d4ac724d9274b7d9eec
Jakob <36012717+jakobfischer17@users.noreply.github.com>
2019-01-16 22:21:13 +0100

    Create 1.3.scm

---
+++ 1.3.scm
@@ -0,0 +1,5 @@
+(define (largestsum x y z)
+  (cond (and (> x y) (> x z) (> y z) (+(* x x) (* y y)))
+        (and (> y z) (> y x) (> z x) (+(* y y) (* z z)))
+        (and (> x y) (> x z) (> z y) (+(* x x) (* z z)))
+  ))
