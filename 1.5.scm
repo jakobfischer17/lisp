2a9c5f89c931a594c6ebdf75b4fec6178c18c7a4
Jakob <36012717+jakobfischer17@users.noreply.github.com>
2020-03-23 17:50:37 +0100

    Update 1.5.scm

--- 1.5.scm
+++ 1.5.scm
@@ -11,8 +11,9 @@ Normal order:
 
 Applicative order:
 (test 0 (p))
-(if (= 0 0) 0 p)
-=> 0
+(test 0 (p))
+(test 0 (p))
+=> never terminates
 
-Same result, but in normal order the operator test gets expanded first, until only primitive procedures calls remain (or in this case if). 
-For applicative order, the arguments get evaluated first and then applied.
+In normal order the operator test gets expanded first, until only primitive procedures calls remain (or in this case if). 
+For applicative order, the arguments get evaluated first and then applied. Since p cannot get expanded further, it will never stop.
