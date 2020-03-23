0d996e09b64bc5dbc4cffb862a778a6203501617
Jakob <36012717+jakobfischer17@users.noreply.github.com>
2020-03-23 17:13:36 +0100

    Create 1.4.scm

---
+++ 1.4.scm
@@ -0,0 +1,4 @@
+(define (a-plus-abs-b a b)
+((if (> b 0) + -) a b))
+
+=> The procedure will calculate a + b if b is greater than 0. Otherwise it will calculate a - b. A and b are operators whose operands are expected to be numbers.
