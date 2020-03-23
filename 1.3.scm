7766694627bd27b8155f3e60164bd5f70ec78e90
Jakob <36012717+jakobfischer17@users.noreply.github.com>
2020-03-23 17:41:39 +0100

    Update 1.3.scm

--- 1.3.scm
+++ 1.3.scm
@@ -3,3 +3,11 @@
         (and (> y z) (> y x) (> z x) (+(* y y) (* z z)))
         (and (> x y) (> x z) (> z y) (+(* x x) (* z z)))
   ))
+
+or
+
+(define (square x) (* x x))
+(define (sqsumlargest x y z)
+(cond ((and  (> x y) (> z y)) (+ (square x) (square z)))
+      ((and  (> x z) (> y z)) (+ (square x) (square y)))
+      ((and  (> y x) (> z x)) (+ (square z) (square y)))))
